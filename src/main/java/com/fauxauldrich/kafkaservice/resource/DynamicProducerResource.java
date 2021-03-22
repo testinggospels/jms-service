06e9b0ed97d28bbf569d31659a6a3304d5183ccf
EC2 Default User <ec2-user@ip-172-31-2-15.ap-south-1.compute.internal>
2021-03-22 05:51:27 +0000

    Split DynamicProducer Resource into controller and service

--- src/main/java/com/fauxauldrich/kafkaservice/resource/DynamicProducerResource.java
+++ src/main/java/com/fauxauldrich/kafkaservice/resource/DynamicProducerResource.java
@@ -1,23 +1,13 @@
 package com.fauxauldrich.kafkaservice.resource;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.Properties;
 
 import com.fauxauldrich.kafkaservice.model.DynamicProducerModel;
 import com.fauxauldrich.kafkaservice.model.DynamicProducerModelWithHeaders;
+import com.fauxauldrich.kafkaservice.service.DynamicProducerService;
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
 
-import org.apache.kafka.clients.CommonClientConfigs;
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
-import org.apache.kafka.common.config.SslConfigs;
-import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,40 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class DynamicProducerResource {
 
     @Autowired
+    private DynamicProducerService producerService;
+
+    @Autowired
     private Handlebars handebarsConfig;
 
     @PostMapping("/publish")
     public String postSimple(@RequestParam("topic") String TOPIC, @RequestParam(required = false) String KEY,
             @RequestParam(required = false) Integer PARTITION_ID, @RequestBody DynamicProducerModel payload)
             throws IOException {
+
         Template template = handebarsConfig.compileInline(payload.getMessage());
-        Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, payload.getBrokers());
-        props.put(ProducerConfig.ACKS_CONFIG, "all");
-        props.put(ProducerConfig.RETRIES_CONFIG, 0);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                "org.apache.kafka.common.serialization.StringSerializer");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-        Producer<String, String> producer = new KafkaProducer<String, String>(props);
-        ProducerRecord<String, String> record;
+        producerService.publish(payload.getBrokers(), TOPIC, KEY, template.apply(""), PARTITION_ID);
 
-        if (KEY != null) {
-            if (PARTITION_ID != null) {
-                record = new ProducerRecord<>(TOPIC, PARTITION_ID, KEY, template.apply(""));
-            } else {
-                record = new ProducerRecord<>(TOPIC, KEY, template.apply(""));
-            }
-        } else {
-            record = new ProducerRecord<>(TOPIC, PARTITION_ID, KEY, template.apply(""));
-        }
-        producer.send(record, new Callback() {
-            @Override
-            public void onCompletion(RecordMetadata metadata, Exception exception) {
-                producer.flush();
-                producer.close();
-            }
-        });
         return "Published successfully";
     }
 
@@ -71,36 +41,12 @@ public class DynamicProducerResource {
     public String postSimpleWithHeaders(@RequestParam("topic") String TOPIC, @RequestParam(required = false) String KEY,
             @RequestParam(required = false) Integer PARTITION_ID, @RequestBody DynamicProducerModelWithHeaders payload)
             throws IOException {
+
         Template template = handebarsConfig.compileInline(payload.getMessage());
-        Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, payload.getBrokers());
-        props.put(ProducerConfig.ACKS_CONFIG, "all");
-        props.put(ProducerConfig.RETRIES_CONFIG, 0);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                "org.apache.kafka.common.serialization.StringSerializer");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-        Producer<String, String> producer = new KafkaProducer<String, String>(props);
-        ProducerRecord<String, String> record;
+        producerService.publishWithHeaders(payload.getBrokers(), TOPIC, KEY, template.apply(""), PARTITION_ID,
+                payload.getHeaders());
 
-        if (KEY != null) {
-            if (PARTITION_ID != null) {
-                record = new ProducerRecord<>(TOPIC, PARTITION_ID, KEY, template.apply(""));
-            } else {
-                record = new ProducerRecord<>(TOPIC, KEY, template.apply(""));
-            }
-        } else {
-            record = new ProducerRecord<>(TOPIC, PARTITION_ID, KEY, template.apply(""));
-        }
-        for (Map.Entry<String, String> entry : payload.getHeaders().entrySet())
-            record.headers().add(new RecordHeader(entry.getKey(), entry.getValue().getBytes()));
-        producer.send(record, new Callback() {
-            @Override
-            public void onCompletion(RecordMetadata metadata, Exception exception) {
-                producer.flush();
-                producer.close();
-            }
-        });
         return "Published successfully";
     }
 
@@ -109,37 +55,12 @@ public class DynamicProducerResource {
             @RequestParam() String TRUSTSTORE_PASSWORD, @RequestParam(required = false) String KEY,
             @RequestParam(required = false) Integer PARTITION_ID, @RequestBody DynamicProducerModel payload)
             throws IOException {
+
         Template template = handebarsConfig.compileInline(payload.getMessage());
-        Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, payload.getBrokers());
-        props.put(ProducerConfig.ACKS_CONFIG, "all");
-        props.put(ProducerConfig.RETRIES_CONFIG, 0);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                "org.apache.kafka.common.serialization.StringSerializer");
-        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
-        props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, TRUSTSTORE_LOCATION);
-        props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, TRUSTSTORE_PASSWORD);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-        Producer<String, String> producer = new KafkaProducer<String, String>(props);
-        ProducerRecord<String, String> record;
+        producerService.publishSecure(payload.getBrokers(), TOPIC, KEY, template.apply(""), PARTITION_ID,
+                TRUSTSTORE_LOCATION, TRUSTSTORE_PASSWORD);
 
-        if (KEY != null) {
-            if (PARTITION_ID != null) {
-                record = new ProducerRecord<>(TOPIC, PARTITION_ID, KEY, template.apply(""));
-            } else {
-                record = new ProducerRecord<>(TOPIC, KEY, template.apply(""));
-            }
-        } else {
-            record = new ProducerRecord<>(TOPIC, PARTITION_ID, KEY, template.apply(""));
-        }
-        producer.send(record, new Callback() {
-            @Override
-            public void onCompletion(RecordMetadata metadata, Exception exception) {
-                producer.flush();
-                producer.close();
-            }
-        });
         return "Published successfully";
     }
 
@@ -148,39 +69,12 @@ public class DynamicProducerResource {
             @RequestParam() String TRUSTSTORE_PASSWORD, @RequestParam(required = false) String KEY,
             @RequestParam(required = false) Integer PARTITION_ID, @RequestBody DynamicProducerModelWithHeaders payload)
             throws IOException {
+
         Template template = handebarsConfig.compileInline(payload.getMessage());
-        Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, payload.getBrokers());
-        props.put(ProducerConfig.ACKS_CONFIG, "all");
-        props.put(ProducerConfig.RETRIES_CONFIG, 0);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                "org.apache.kafka.common.serialization.StringSerializer");
-        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
-        props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, TRUSTSTORE_LOCATION);
-        props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, TRUSTSTORE_PASSWORD);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-        Producer<String, String> producer = new KafkaProducer<String, String>(props);
-        ProducerRecord<String, String> record;
+        producerService.publishSecureWithHeaders(payload.getBrokers(), TOPIC, KEY, template.apply(""), PARTITION_ID,
+                TRUSTSTORE_LOCATION, TRUSTSTORE_PASSWORD, payload.getHeaders());
 
-        if (KEY != null) {
-            if (PARTITION_ID != null) {
-                record = new ProducerRecord<>(TOPIC, PARTITION_ID, KEY, template.apply(""));
-            } else {
-                record = new ProducerRecord<>(TOPIC, KEY, template.apply(""));
-            }
-        } else {
-            record = new ProducerRecord<>(TOPIC, PARTITION_ID, KEY, template.apply(""));
-        }
-        for (Map.Entry<String, String> entry : payload.getHeaders().entrySet())
-            record.headers().add(new RecordHeader(entry.getKey(), entry.getValue().getBytes()));
-        producer.send(record, new Callback() {
-            @Override
-            public void onCompletion(RecordMetadata metadata, Exception exception) {
-                producer.flush();
-                producer.close();
-            }
-        });
         return "Published successfully";
     }
 }
